8d2cbc42455e06aa69a3627cd876fc17f00e98d6
Andrew Poydence <apoydence@gmail.com>
2017-08-20 05:48:12 -0600

    Adds ranges and gets in maps benchmark

--- maps_test.go
+++ maps_test.go
@@ -46,6 +46,13 @@ func BenchmarkMapInterfaceString(b *testing.B) {
 	}
 }
 
+func BenchmarkMapNil(b *testing.B) {
+	m := make(map[interface{}]struct{})
+	for i := 0; i < b.N; i++ {
+		m[nil] = struct{}{}
+	}
+}
+
 func BenchmarkMapInterfaceBoth(b *testing.B) {
 	m := make(map[interface{}]struct{})
 	for i := 0; i < b.N; i++ {
@@ -54,6 +61,221 @@ func BenchmarkMapInterfaceBoth(b *testing.B) {
 			m[randStrs[i%len(randStrs)]] = struct{}{}
 		case 1:
 			m[i%1000] = struct{}{}
+		case 2:
+			m[nil] = struct{}{}
 		}
 	}
+}
+
+func BenchmarkMapIntGet(b *testing.B) {
+	m := make(map[int]struct{})
+	for i := 0; i < 20000; i++ {
+		m[i%1000] = struct{}{}
+	}
+
+	b.ResetTimer()
+
+	var result struct{}
+	for i := 0; i < b.N; i++ {
+		result = m[i%1000]
+	}
+
+	_ = result
+}
+
+func BenchmarkMapStringGet(b *testing.B) {
+	m := make(map[string]struct{})
+	for i := 0; i < 20000; i++ {
+		m[randStrs[i%len(randStrs)]] = struct{}{}
+	}
+
+	b.ResetTimer()
+
+	var result struct{}
+	for i := 0; i < b.N; i++ {
+		result = m[randStrs[i%len(randStrs)]]
+	}
+
+	_ = result
+}
+
+func BenchmarkMapInterfaceIntGet(b *testing.B) {
+	m := make(map[interface{}]struct{})
+	for i := 0; i < 20000; i++ {
+		m[i%1000] = struct{}{}
+	}
+
+	b.ResetTimer()
+
+	var result struct{}
+	for i := 0; i < b.N; i++ {
+		result = m[i%1000]
+	}
+
+	_ = result
+}
+
+func BenchmarkMapInterfaceStringGet(b *testing.B) {
+	m := make(map[interface{}]struct{})
+	for i := 0; i < 20000; i++ {
+		m[randStrs[i%len(randStrs)]] = struct{}{}
+	}
+
+	b.ResetTimer()
+
+	var result struct{}
+	for i := 0; i < b.N; i++ {
+		result = m[randStrs[i%len(randStrs)]]
+	}
+
+	_ = result
+}
+
+func BenchmarkMapNilGet(b *testing.B) {
+	m := make(map[interface{}]struct{})
+	for i := 0; i < 20000; i++ {
+		m[nil] = struct{}{}
+	}
+
+	b.ResetTimer()
+
+	var result struct{}
+	for i := 0; i < b.N; i++ {
+		result = m[nil]
+	}
+
+	_ = result
+}
+
+func BenchmarkMapInterfaceBothGet(b *testing.B) {
+	m := make(map[interface{}]struct{})
+	for i := 0; i < 20000; i++ {
+		switch i % 2 {
+		case 0:
+			m[randStrs[i%len(randStrs)]] = struct{}{}
+		case 1:
+			m[i%1000] = struct{}{}
+		case 2:
+			m[nil] = struct{}{}
+		}
+	}
+
+	b.ResetTimer()
+
+	var result struct{}
+
+	for i := 0; i < b.N; i++ {
+		switch i % 2 {
+		case 0:
+			result = m[randStrs[i%len(randStrs)]]
+		case 1:
+			result = m[i%1000]
+		case 2:
+			result = m[nil]
+		}
+	}
+
+	_ = result
+}
+
+func BenchmarkMapIntRange(b *testing.B) {
+	m := make(map[int]struct{})
+	for i := 0; i < 5; i++ {
+		m[i%1000] = struct{}{}
+	}
+
+	b.ResetTimer()
+	var keyResult int
+	var valueResult struct{}
+	for i := 0; i < b.N; i++ {
+		for k, v := range m {
+			keyResult = k
+			valueResult = v
+		}
+	}
+	_ = keyResult
+	_ = valueResult
+}
+
+func BenchmarkMapStringRange(b *testing.B) {
+	m := make(map[string]struct{})
+	for i := 0; i < 5; i++ {
+		m[randStrs[i%len(randStrs)]] = struct{}{}
+	}
+
+	b.ResetTimer()
+	var keyResult string
+	var valueResult struct{}
+	for i := 0; i < b.N; i++ {
+		for k, v := range m {
+			keyResult = k
+			valueResult = v
+		}
+	}
+	_ = keyResult
+	_ = valueResult
+}
+
+func BenchmarkMapInterfaceIntRange(b *testing.B) {
+	m := make(map[interface{}]struct{})
+	for i := 0; i < 5; i++ {
+		m[i%1000] = struct{}{}
+	}
+
+	b.ResetTimer()
+	var keyResult interface{}
+	var valueResult struct{}
+	for i := 0; i < b.N; i++ {
+		for k, v := range m {
+			keyResult = k
+			valueResult = v
+		}
+	}
+	_ = keyResult
+	_ = valueResult
+}
+
+func BenchmarkMapInterfaceStringRange(b *testing.B) {
+	m := make(map[interface{}]struct{})
+	for i := 0; i < 5; i++ {
+		m[randStrs[i%len(randStrs)]] = struct{}{}
+	}
+
+	b.ResetTimer()
+	var keyResult interface{}
+	var valueResult struct{}
+	for i := 0; i < b.N; i++ {
+		for k, v := range m {
+			keyResult = k
+			valueResult = v
+		}
+	}
+	_ = keyResult
+	_ = valueResult
+}
+
+func BenchmarkMapInterfaceBothRange(b *testing.B) {
+	m := make(map[interface{}]struct{})
+	for i := 0; i < 5; i++ {
+		switch i % 2 {
+		case 0:
+			m[randStrs[i%len(randStrs)]] = struct{}{}
+		case 1:
+			m[i%1000] = struct{}{}
+		case 2:
+			m[nil] = struct{}{}
+		}
+	}
+
+	b.ResetTimer()
+	var keyResult interface{}
+	var valueResult struct{}
+	for i := 0; i < b.N; i++ {
+		for k, v := range m {
+			keyResult = k
+			valueResult = v
+		}
+	}
+	_ = keyResult
+	_ = valueResult
 }
