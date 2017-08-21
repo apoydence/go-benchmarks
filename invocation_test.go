e7c92440616f9aae88ac1c6fe019b2c98705fa9b
Andrew Poydence <apoydence@pivotal.io>
2017-08-21 16:00:50 -0600

    Add more profiles to invocation

--- invocation_test.go
+++ invocation_test.go
@@ -20,13 +20,13 @@ func foo() {
 	i++
 }
 
-func BenchmarkFunction(b *testing.B) {
+func BenchmarkInvocationFunction(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		foo()
 	}
 }
 
-func BenchmarkClosure(b *testing.B) {
+func BenchmarkInvocationClosure(b *testing.B) {
 	var i int
 	f := func() {
 		i++
@@ -36,21 +36,21 @@ func BenchmarkClosure(b *testing.B) {
 	}
 }
 
-func BenchmarkConcrete(b *testing.B) {
+func BenchmarkInvocationConcrete(b *testing.B) {
 	a := new(A)
 	for i := 0; i < b.N; i++ {
 		a.A()
 	}
 }
 
-func BenchmarkInterface(b *testing.B) {
+func BenchmarkInvocationInterface(b *testing.B) {
 	var a InterfaceA = new(A)
 	for i := 0; i < b.N; i++ {
 		a.A()
 	}
 }
 
-func BenchmarkConcreteFunc(b *testing.B) {
+func BenchmarkInvocationConcreteFunc(b *testing.B) {
 	a := new(A)
 	f := a.A
 	for i := 0; i < b.N; i++ {
@@ -58,10 +58,85 @@ func BenchmarkConcreteFunc(b *testing.B) {
 	}
 }
 
-func BenchmarkInterfaceFunc(b *testing.B) {
+func BenchmarkInvocationInterfaceFunc(b *testing.B) {
 	var a InterfaceA = new(A)
 	f := a.A
 	for i := 0; i < b.N; i++ {
+		f()
+	}
+}
+
+func bar(x int) {
+	i = x
+}
+
+type InterfaceB interface {
+	B(j int)
+}
+
+type B struct {
+	j int
+}
+
+func (b *B) B(j int) {
+	b.j = j
+}
+
+func BenchmarkInvocationFunctionWithArgs(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		bar(i)
+	}
+}
+
+func BenchmarkInvocationClosureWithArgs(b *testing.B) {
+	var i int
+	f := func(x int) {
+		i = x
+	}
+	for i := 0; i < b.N; i++ {
+		f(i)
+	}
+}
+
+func BenchmarkInvocationConcreteWithArgs(b *testing.B) {
+	bb := new(B)
+	for i := 0; i < b.N; i++ {
+		bb.B(i)
+	}
+}
+
+func BenchmarkInvocationInterfaceWithArgs(b *testing.B) {
+	var bb InterfaceB = new(B)
+	for i := 0; i < b.N; i++ {
+		bb.B(i)
+	}
+}
+
+func BenchmarkInvocationConcreteFuncWithArgs(b *testing.B) {
+	bb := new(B)
+	f := bb.B
+	for i := 0; i < b.N; i++ {
+		f(i)
+	}
+}
+
+func BenchmarkInvocationInterfaceFuncWithArgs(b *testing.B) {
+	var bb InterfaceB = new(B)
+	f := bb.B
+	for i := 0; i < b.N; i++ {
+		f(i)
+	}
+}
+
+func functionFactory(j int) func() {
+	return func() {
+		i = j
+	}
+}
+
+func BenchmarkInvocationFactoryFunctionWithArgs(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		f := functionFactory(i)
 		f()
 	}
 }
